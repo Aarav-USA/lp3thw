4d1b62634427092f9987e658fb1f13cee2f97103
Aarav Batra <80801287+Aarav-Batra@users.noreply.github.com>
2021-12-29 05:17:09 +0000

    12/29/2021 Progress
    Starting off with Python printing, variables, syntax, and math.

---
+++ ex3_study_drills.py
@@ -0,0 +1,4 @@
+print(9 + 10)
+print("The meme says it 21!")
+print("Python 3 is the newest version so it may be right.")
+print("I mean that the internet is not true all the time which is why python may be right.")
