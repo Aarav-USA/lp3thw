2b977132dbc2900176a80971d38b84c359ec4041
Aarav Batra <80801287+Aarav-Batra@users.noreply.github.com>
2022-01-01 13:05:15 -0500

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,14 +1,14 @@
 name = 'Zed A. Shaw'
 age = 35 # not a lie
-height = 74 # inches
-weight = 180 # lbs
+height = round(74 * 2.54) # centimeters
+weight = round(180 * 0.45359237) # kilograms
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 print(f"Let's talk about {name}.")
-print(f"He's {height} inches tall.")
-print(f"He's {weight} pounds heavy.")
+print(f"He's {height} centimeters tall.")
+print(f"He's {weight} kilograms heavy.")
 print(f"Actually that's not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee,")
@@ -16,5 +16,3 @@ print(f"His teeth are usually {teeth} depending on the coffee,")
 # this line is tricky, try to get it exactly right
 total = age + height + weight
 print(f"If I add {age}, {height}, and {weight} I get {total}.")
-
-# Change inches and pounds to centimeters and kilograms
