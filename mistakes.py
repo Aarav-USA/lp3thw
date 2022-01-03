dd6d9bfe6dd49e340831093099bab73e17c3743e
Aarav Batra <80801287+Aarav-Batra@users.noreply.github.com>
2022-01-02 20:00:15 -0500

    Update mistakes.py

--- mistakes.py
+++ mistakes.py
@@ -1,3 +1,4 @@
 print('''
 Added space before equal sign on var end, to raw line 20 or line 38, ex7 and returned syntax error.
+Wrote CheeseBurger in the comments instead of Cheese or Burger for var end, more specific on lines 10-33 ex7.
 ''')
