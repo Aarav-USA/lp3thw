b88a22bd9d0e00177f04d6b4dda5a558a3aa48f0
Aarav Batra <80801287+Aarav-Batra@users.noreply.github.com>
2022-01-16 22:41:20 -0500

    Added exercises 8 and 9, updated mistakes.py

---
+++ ex9.py
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remember type it exactly
+
+# define each day
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# define most of the months
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# print defined days
+print("Here are the days: ", days)
+# print defined months
+print("Here are the months: ", months)
+
+# print the process of figuring out multi-line print syntax
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
--- mistakes.py
+++ mistakes.py
@@ -1,4 +1,5 @@
 print('''
 Added space before equal sign on var end, to raw line 20 or line 38, ex7 and returned syntax error.
 Wrote CheeseBurger in the comments instead of Cheese or Burger for var end, more specific on lines 10-33 ex7.
+Forgot comma on raw line 6 of ex8 or line 11.
 ''')
