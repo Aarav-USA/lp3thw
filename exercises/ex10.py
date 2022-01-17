192dd96837b845fd2e3796529aa9e36927b30d5e
Aarav Batra <80801287+Aarav-Batra@users.noreply.github.com>
2022-01-17 17:17:26 -0500

    Completed exercises 10-12, and updates mistakes.py

---
+++ exercises/ex10.py
@@ -0,0 +1,12 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list\n \t* Cat Food\n \t* Fishies\n \t* Catnip\n\t* Grass
+'''
+
+print("{}".format(tabby_cat))
+print("{}".format(persian_cat))
+print("{}".format(backslash_cat))
+print("{}".format(fat_cat))
