82a25823926a2cb6d7deb3bf1c8442ab9ac76937
Aarav-USA <aarav-usa@pm.me>
2022-04-08 20:36:52 -0400

    Restart

--- ex/13.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-# read the WYSS section for how to run this
-script, first, second, third, fourth = argv
-
-print("The script is called:", script)
-input("Your first variable is: " + first + "?")
-input("Your second variable is: " + second + "?")
-input("Your third variable is: " + third + "?")
-input("Your fourth variable is: " + fourth + "?")
--- sd/mistakes.py
+++
@@ -1,9 +0,0 @@
-print('''
-Added space before equal sign on var end, to raw line 20 or line 38, ex7 and returned syntax error.
-Wrote CheeseBurger in the comments instead of Cheese or Burger for var end, more specific on lines 10-33 ex7.
-Forgot comma on raw line 6 of ex8 or line 11.
-Misdefined variable "blackslash_cat" instead of "backslash_cat" ex10 raw line 15
-Forgot double quotes for "{\}" on line 12 ex10
-Forgot to add "f" string on line 12 of ex11 study drills, and had grammar mistakes in line 12.
-Wrote weight instead of weight in line 3 ex12
-''')
